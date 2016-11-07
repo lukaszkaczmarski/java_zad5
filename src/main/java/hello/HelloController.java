ac766725a8f4e81785fd1108d030bdcb7c3c7b47
Łukasz Kaczmarski <lukasz.kaczmarski93@gmail.com>
2016-11-07 12:34:00 +0100

    Fix

--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -7,6 +7,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Controller
@@ -31,7 +32,7 @@ public class HelloController {
         String [] fields = { "id", "about", "age_range", "birthday", "context", "cover", "currency", "devices", "education", "email", "favorite_athletes", "favorite_teams", "first_name", "gender", "hometown", "inspirational_people", "installed", "install_type", "is_verified", "languages", "last_name", "link", "locale", "location", "meeting_for", "middle_name", "name", "name_format", "political", "quotes", "payment_pricepoints", "relationship_status", "religion", "security_settings", "significant_other", "sports", "test_group", "timezone", "third_party_id", "updated_time", "verified", "video_upload_limits", "viewer_can_send_gift", "website", "work" };
         User userprofile = facebook.fetchObject("me", User.class, fields);
         List<EducationExperience> edu = userprofile.getEducation();
-        List<String> list = null;
+        List<String> list = new ArrayList<>();
         for (EducationExperience i: edu)
         {
             String a = i.getSchool().getName();
