fc5e2f195bfc5361185f343868e721fdb4f36d21
Łukasz Kaczmarski <lukasz.kaczmarski93@gmail.com>
2016-11-07 11:24:30 +0100

    Init

---
+++ src/main/java/hello/HelloController.java
@@ -0,0 +1,43 @@
+package hello;
+
+import org.springframework.social.connect.ConnectionRepository;
+import org.springframework.social.facebook.api.*;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/")
+public class HelloController {
+
+    private Facebook facebook;
+    private ConnectionRepository connectionRepository;
+
+    public HelloController(Facebook facebook, ConnectionRepository connectionRepository) {
+        this.facebook = facebook;
+        this.connectionRepository = connectionRepository;
+    }
+
+    @GetMapping
+    public String helloFacebook(Model model) {
+        if (connectionRepository.findPrimaryConnection(Facebook.class) == null) {
+            return "redirect:/connect/facebook";
+        }
+
+        String [] field = { "education" };
+        String [] fields = { "id", "about", "age_range", "birthday", "context", "cover", "currency", "devices", "education", "email", "favorite_athletes", "favorite_teams", "first_name", "gender", "hometown", "inspirational_people", "installed", "install_type", "is_verified", "languages", "last_name", "link", "locale", "location", "meeting_for", "middle_name", "name", "name_format", "political", "quotes", "payment_pricepoints", "relationship_status", "religion", "security_settings", "significant_other", "sports", "test_group", "timezone", "third_party_id", "updated_time", "verified", "video_upload_limits", "viewer_can_send_gift", "website", "work" };
+        User userprofile = facebook.fetchObject("me", User.class, fields);
+        List<EducationExperience> edu = userprofile.getEducation();
+        List<String> list = null;
+        for (EducationExperience i: edu)
+        {
+            String a = i.getSchool().getName();
+            list.add(a);
+        }
+        model.addAttribute("edu",list);
+        return "hello";
+    }
+}
